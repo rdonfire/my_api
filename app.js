dfb3bcf0859dfe11d75a011e88e315a6f6c641e3
rdonfire <134340430+rdonfire@users.noreply.github.com>
2024-03-03 18:35:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,48 +21,52 @@ connection.connect((err) => {
     console.log('Conexão bem-sucedida ao banco de dados MySQL');
 });
 
-// Rota para obter todos os usuários
-app.get('/api/users', (req, res) => {
-    connection.query('SELECT * FROM clientes', (err, results) => {
+app.post('/api/addclientes', (req, res) =>{
+    const {nome, telefone } = req.body;
+    const query = 'INSERT INTO clientes (nome, telefone) VALUES (?,?)';
+    connection.query(query, [nome, telefone], (err,result) => {
+        if(err) {
+            console.error('Erro ao adicionar cliente: ', err);
+            res.status(500).json({error: 'Erro interno do Servidor' });
+            return;
+        }
+        res.status(201).json({ ud: result.insertId, nome, telefone});
+    });    
+    
+}); 
+
+app.post('/api/addservicos', (req, res) => {
+    const { servico, preco } = req.body;
+    const query = 'INSERT INTO servicos (servico, preco) VALUES (?, ?)';
+    connection.query(query, [servico, preco], (err, result) => {
         if (err) {
-            console.error('Erro ao executar a consulta:', err);
+            console.error('Erro ao adicionar serviço:', err);
             res.status(500).json({ error: 'Erro interno do servidor' });
             return;
         }
-        res.json(results);
+        res.status(201).json({ id: result.insertId, servico, preco });
     });
 });
+
+app.post('/api/addbarbeiros', (req, res) => {
+    const { nome } = req.body;
+    const query = 'INSERT INTO barbers (nome) VALUES (?)';
+    connection.query(query, [nome], (err, result) => {
+        if (err) {
+            console.error('Erro ao adicionar barbeiro:', err);
+            res.status(500).json({ error: 'Erro interno do servidor' });
+            return;
+        }
+        res.status(201).json({ id: result.insertId, nome });
+    });
+});
+
+
+
 
 // Encerrar a conexão com o banco de dados quando a aplicação é encerrada
 process.on('SIGINT', () => {
     connection.end();
     process.exit();
 });
- /*
-
-// Middleware para analisar solicitações com corpo JSON
-app.use(bodyParser.json());
-
-// Dados de exemplo (simulando um banco de dados)
-let users = [
-    { id: 1, name: 'TAMO JUNTO MEU MANO CAIO' },
-    
-];
-
-// Rota para obter todos os usuários
-app.get('/api/users', (req, res) => {
-    res.json(users);
-});
-
-// Rota para adicionar um novo usuário
-app.post('/api/users', (req, res) => {
-    const newUser = req.body;
-    users.push(newUser);
-    res.status(201).json(newUser);
-});
-
-// Iniciar o servidor
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor rodando no arquivo app.js em http://localhost:${PORT}`);
-});
-*/
+ 
